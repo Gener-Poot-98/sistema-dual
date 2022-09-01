ce5aa5bf2b116ef3afefd9a65d62e7a74eada549
JonathanOrtega15 <jonathan.ortegacanul1@gmail.com>
2022-09-01 10:53:39 -0500

    Editanto vistas del layout

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'name' => 'Home',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
